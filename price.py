fea5122fb90d490702e71b137d06bcbc2049fe66
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-12-07 17:07:59 -0700

    Update price.py

--- price.py
+++ price.py
@@ -1,10 +1,14 @@
 import os
+import ctypes
 import yfinance as yf
 
 # Resize the terminal window
 if os.name == "nt":
     # Windows
     os.system("mode con: cols=35 lines=15")
+
+# Rename terminal window
+ctypes.windll.kernel32.SetConsoleTitleW("GME Price - [Live]")
 
 gme = yf.Ticker("GME")
 
